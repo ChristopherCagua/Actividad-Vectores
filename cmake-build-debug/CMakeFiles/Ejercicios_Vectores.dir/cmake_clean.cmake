195b41ded8482f2c0550c90e8ae6ca0c7b257404
ChristopherCagua <caguachristopher@gmail.com>
2021-08-03 00:45:01 -0500

    Actividad sobre Vectores

---
+++ cmake-build-debug/CMakeFiles/Ejercicios_Vectores.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejercicios_Vectores.dir/main.cpp.obj"
+  "Ejercicios_Vectores.exe"
+  "Ejercicios_Vectores.exe.manifest"
+  "Ejercicios_Vectores.pdb"
+  "libEjercicios_Vectores.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejercicios_Vectores.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
